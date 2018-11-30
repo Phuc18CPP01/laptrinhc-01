0ef15eb0d8e562fd498ee794e03cca6953b1bf6f
= <=>
2018-11-30 19:56:52 +0700

    add

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -20,4 +20,56 @@ int main() {
 		cout << i << "\t";
 	}
 	//Tinh tong cac so tu 1 -> 100
+	int sum = 0;
+	for(int i = 1; i <= 100; i++) {
+		sum += i; //sum = sum + i;
+	}
+	cout << sum;
+	//Tinh tong cac so chan tu 1 -> 100
+	int sum = 0;
+	for(int i = 2; i <= 100; i+=2) {
+		sum += i; //sum = sum + i;
+	}
+	cout << sum;
+	//Tinh tong cac so le tu 1 -> 100
+	int sum = 0;
+	for(int i = 1; i <= 100; i+=2) {
+		sum += i; //sum = sum + i;
+	}
+	cout << sum;
+	//Xuat cac so tu 1 den n va n nhap tu ban 
+	//phim	
+	int n;
+	cin >> n;
+	for(int i = 1; i <= n; i++) {
+		cout << i << endl;
+	}
+	//Tinh tong cac so tu 1 den n
+	int n, sum = 0;
+	cin >> n;
+	for(int i = 1; i <= n; i++) {
+		sum += i; //sum = sum + i;
+	}
+	cout << sum;
+	//Tinh tong cac so le tu 1 den n
+	int n, sum = 0;
+	cin >> n;
+	for(int i = 1; i <= n; i+=2) {
+		sum += i; //sum = sum + i;
+	}
+	cout << sum;
+	//Tinh tong cac so chan tu 1 den n
+	int n, sum = 0;
+	cin >> n;
+	for(int i = 2; i <= n; i+=2) {
+		sum += i; //sum = sum + i;
+	}
+	cout << sum;
+	//Tinh tich cac so tu 1 den n
+	int n, sum = 1;
+	cin >> n;
+	for(int i = 1; i <= n; i++) {
+		sum *= i; //sum = sum + i;
+	}
+	cout << sum;
 }
